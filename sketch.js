87388801523bcbbe3c616523b39f89ef4cee63f4
GustavoXlh3 <gustavoxlh3@gmail.com>
2022-03-16 20:08:33 -0300

    criando corda e fruta

--- sketch.js
+++ sketch.js
@@ -10,6 +10,8 @@ const Composite = Matter.Composite;
 let engine;
 let world;
 var ground;
+var fruit, rope;
+var connectFruit;
 
 function setup() 
 {
@@ -18,18 +20,26 @@ function setup()
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(200,490,600,20);
+  rope = new Rope(5, {x:245, y:30});
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
   textSize(50)
   
+   fruit = Bodies.circle(300, 300, 20);
+   //World.add(world, fruit);
+
+   Composite.add (rope.body, fruit);
+   connectFruit = new Link(rope, fruit);
 }
 
 function draw() 
 {
   background(51);
   ground.show();
+  rope.show();
+
+  ellipse(fruit.position.x, fruit.position.y, 20, 20);
   
   Engine.update(engine);
-   
 }
